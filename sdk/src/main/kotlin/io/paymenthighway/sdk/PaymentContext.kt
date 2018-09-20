7732d5867c48e6b262048c5749af618724a7d462
Stefano Pironato <stefano.pironato@paymenthighway.fi>
2018-09-20 12:59:40 +0300

    Small refactoring in PaymentContext

--- sdk/src/main/kotlin/io/paymenthighway/sdk/PaymentContext.kt
+++ sdk/src/main/kotlin/io/paymenthighway/sdk/PaymentContext.kt
@@ -1,6 +1,5 @@
 package io.paymenthighway.sdk
 
-import io.paymenthighway.sdk.exception.InternalErrorException
 import io.paymenthighway.sdk.model.*
 import io.paymenthighway.sdk.service.PaymentHighwayService
 import io.paymenthighway.sdk.util.Result
@@ -34,27 +33,25 @@ class PaymentContext<V>(config: PaymentConfig, private val backendAdapter: Backe
     fun addCard(card: CardData, completion: (Result<V, Exception>) -> Unit) {
         backendAdapter.getTransactionId { result ->
             when (result) {
-                is Result.Success -> getTransactionIdHandler(result.value, card, completion)
+                is Result.Success -> getEncryptionKey(result.value, card, completion)
                 is Result.Failure -> completion(result)
             }
         }
     }
 
-    private fun getTransactionIdHandler(transactionId: TransactionId, card: CardData, completion: (Result<V, Exception>) -> Unit ) {
+    private fun getEncryptionKey(transactionId: TransactionId, card: CardData, completion: (Result<V, Exception>) -> Unit ) {
         service.encryptionKey(transactionId) { result ->
             when (result) {
-                is Result.Success -> getEncryptionKeyHandler(result.value, transactionId, card, completion)
+                is Result.Success -> tokenizeCard(result.value, transactionId, card, completion)
                 is Result.Failure -> completion(result)
             }
         }
     }
 
-    private fun getEncryptionKeyHandler(encryptionKey: EncryptionKey, transactionId: TransactionId, card: CardData, completion: (Result<V, Exception>) -> Unit ) {
+    private fun tokenizeCard(encryptionKey: EncryptionKey, transactionId: TransactionId, card: CardData, completion: (Result<V, Exception>) -> Unit ) {
         service.tokenizeTransaction(transactionId, card, encryptionKey) { result ->
             when (result) {
-                is Result.Success -> {
-                    backendAdapter.addCardCompleted(transactionId, completion)
-                }
+                is Result.Success -> backendAdapter.addCardCompleted(transactionId, completion)
                 is Result.Failure -> completion(result)
             }
         }
